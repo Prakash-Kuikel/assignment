d078b0f6c74b5f1fb4a01e8a981956456431d2e9
PrakashQkel <91109741+PrakashQkel@users.noreply.github.com>
2022-05-27 18:00:21 +0600

    Create, Update and Delete

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -2,5 +2,15 @@ class Types::PostType < Types::BaseObject
 
     field :body, String, null: true
     field :created_at, String, null: true
-    
+
+end
+
+class Types::PostInputType < GraphQL::Schema::InputObject
+
+    graphql_name "postInputType"
+
+    argument :id, ID, required: false
+    argument :user_id, ID, required: false
+    argument :body, String, required: false
+
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -7,4 +7,18 @@ class Types::UserType < Types::BaseObject
     def post
         Post.where(user_id: object.id).all
     end
+
 end
+
+class Types::UserInputType < GraphQL::Schema::InputObject
+
+    graphql_name "UserInputType"
+       
+    argument :id, ID, required: false
+    argument :email, String, required: false
+    argument :name, String, required: false
+    argument :password, String, required: false
+    argument :password_confirmation, String, required: false
+    
+end
+
